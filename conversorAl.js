c9774d24cacce24312f095404ebdc05a972a906e
Bryan <bryan_prinz-silva@estudante.sesisenai.org.br>
2024-02-07 14:56:07 -0300

    conversor para Alumínio 02

--- conversorAl.js
+++ conversorAl.js
@@ -1,4 +1,4 @@
-let resistividade = 0.0172 // 0,0172 Ω. mm²/m condutor de cobre
+let resistividade = 0.0282 // 0,0282 Ω. mm²/m condutor de alumínio
 
 let comprimento = 1555
 let areaTransversal = 2.5 // o valor mínimo é 1 para não dividir por zero
